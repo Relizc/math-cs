3f384239085fb8925e6d6f814ec5ea5b74ac9fde
Relizc <3464840574@qq.com>
2026-05-29 10:37:08 +0800

    latex update

--- main.py
+++ main.py
@@ -9,5 +9,50 @@ from PySide6.QtWidgets import (
     QVBoxLayout,
     QLabel,
     QPushButton,
+    QLineEdit,
+    
 )
 
+from sympy import Symbol, symbols, solve, Eq
+import sympy
+from sympy.parsing.latex import parse_latex
+
+class Window(QMainWindow):
+
+    def __init__(self):
+        main = QWidget()
+        self.setCentralWidget(main)
+
+        layout = QHBoxLayout()
+        main.setLayout(layout) 
+
+        nav = QWidget()
+        nav_layout = QVBoxLayout()
+        nav.setLayout(nav_layout)
+
+        nav.addWidget(QLabel("Enter formula (or copy from desmos)"))
+        nav.addWidget(QLineEdit("2x+y=5"))
+
+def subConstants(latex):
+    return latex.subs(Symbol("e"), sympy.E).subs(Symbol("pi"), sympy.pi)
+
+def solveLatex(latex: str, input, solve_for: str):
+
+    solver = Eq(subConstants(parse_latex(latex.split("=")[0])), subConstants(parse_latex(latex.split("=")[1])))
+
+    symbs = {}
+    if solve_for == "y":
+        symbs["x"] = input
+    if solve_for == "x":
+        symbs["y"] = input
+
+
+    subbed = solver.subs(symbs)
+
+    sol = solve(subbed, symbols(solve_for))
+
+    evaluated = [x.evalf() for x in sol]
+
+    return evaluated
+
+print(solveLatex("e+3=y", -2, "y"))
